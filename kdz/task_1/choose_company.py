bc97e2dd2c0c3fffb957c6e228e619efae93ca79
arubashenkov <arubashenkov@reunico.com>
2020-09-28 02:06:18 +0300

    Сделано первое задание из КДЗ

---
+++ kdz/task_1/choose_company.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+import numpy as np
+
+
+def make_decision(array):
+    A = np.array(array).astype(float)
+    A = A.reshape(4, 4)
+    print 'Матрица A:\n', A
+    decision_matrix = [0, 0, 0, 0]
+    wald_decisions = wald(A)
+    savage_decisions = savage(A)
+    hurwitz_decisions = hurwitz(A)
+    laplace_decisions = laplace(A)
+    decision_matrix = add_decisions(wald_decisions, decision_matrix)
+    decision_matrix = add_decisions(savage_decisions, decision_matrix)
+    decision_matrix = add_decisions(hurwitz_decisions, decision_matrix)
+    decision_matrix = add_decisions(laplace_decisions, decision_matrix)
+    print 'Матрица голосования:\n', decision_matrix
+    maximums = [i for i in range(len(decision_matrix)) if decision_matrix[i] == max(decision_matrix)]
+    print 'Выбрано преприятие: ', maximums
+    if len(maximums) > 1:
+        print 'По результатам вычислений выбрано несколько предприятий, требуется дополнительное исследование.'
+
+
+def wald(A):
+    print 'критерии Вальда'
+    min_a = np.min(A, axis=1)
+    print 'Минимальные значения для каждой строки:', min_a
+    max_in_min_a = max(min_a)
+    print 'Максимальное значение из минимальных значений для каждой строки:', max_in_min_a
+    strategy = np.where(min_a == max_in_min_a)
+    print 'Выбраны стратегии:', strategy[0]
+    return strategy[0]
+
+
+def savage(A):
+    print 'Критерий Севиджа'
+    max_a = np.max(A, axis=0)
+    print 'Максимальные значения для каждого столбца:', max_a
+    R = A.copy()
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            R[j][i] = max_a[i] - R[j][i]
+    print 'Матрица R:\n', R
+    max_a = np.max(R, axis=1)
+    print 'Максимальные значения для каждой строки:', max_a
+    min_in_max_a = min(max_a)
+    print 'Минимальное значение из максимальных значений для каждой строки:', min_in_max_a
+    strategy = np.where(max_a == min_in_max_a)
+    print 'Выбраны стратегии:', strategy[0]
+    return strategy[0]
+
+
+def hurwitz(A):
+    alpha = 0.6
+    print 'Критерий Гурвица'
+    max_a = np.max(A, axis=1)
+    print 'Максимальные значения для каждой строки:', max_a
+    min_a = np.min(A, axis=1)
+    print 'Минимальные значения для каждой строки:', min_a
+    hurwitz_matrix = alpha*min_a + (1 - alpha)*max_a
+    print 'Матрица Гурвица:\n', hurwitz_matrix
+    max_a = max(hurwitz_matrix)
+    print 'Максимальное значение в матрице Гурвица:', max_a
+    strategy = np.where(hurwitz_matrix == max_a)
+    print 'Выбраны стратегии:', strategy[0]
+    return strategy[0]
+
+
+def laplace(A):
+    print 'Критерий Лапласа'
+    n = 4
+    D = A.copy()
+    D = D/n
+    print 'Матрица D\n', D
+    sum_d = np.sum(D, axis=1)
+    print 'Суммы строк матрицы D:', sum_d
+    max_d = max(sum_d)
+    print 'Максимальная сумма строк:', max_d
+    strategy = np.where(sum_d == max_d)
+    print 'Выбраны стратегии:', strategy[0]
+    return strategy[0]
+
+
+def add_decisions(decisions, matrix):
+    for decision in decisions:
+        matrix[decision] = matrix[decision] + 1
+    return matrix
+
+
+make_decision([10, 6, 3, 2, 10, 5, 4, 8, 7, 7, 2, 3, 3, 8, 6, 5])
